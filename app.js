e6fa3c5f2c277eebf878f48ab010de0e30763ebf
Soudip Mondal <soudip.sm2.1750@gmail.com>
2024-09-07 18:32:08 +0530

    Updated app.js to fix view rendering issue

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.all("*",(req,res,next) => {
 
 app.use((err,req,res,next) => {
     let {statusCode=500, message="Something went wrong"} = err;
-    res.status(statusCode).render("error.ejs",{err});
+    res.status(statusCode).render("Error.ejs",{err});
     // res.status(statusCode).send(message);
 });
 
